4c400d31ce47cff98222f8a089d81f09577cca8f
sh3rLock3d <alipourghsaemi>
2020-10-26 13:12:37 +0330

    sha256 part is ready

--- nodejs/index.js
+++ nodejs/index.js
@@ -3,21 +3,49 @@ const app = express()
 var cors = require('cors')
 const { response } = require('express')
 const port = 3000
+const crypto = require('crypto');
+app.use(express.json());
 
 app.use(cors())
 
-app.get('/', (req, res) => {
+
+
+
+
+app.get('/nodejs/write', (req, res) => {
+    // is it better using https://stackoverflow.com/questions/34730586/read-nth-line-in-node-js-without-reading-entire-file
     console.log('hello ')
-    res.json({msg: 'some message for get'})
+    var line = 1 // todo find how?
+    console.log(tmp)
+    res.json({msg: 'not implemented yet'})
 })
 
 app.post('/nodejs/sha256', function (req, res) {
     //res.send('POST request to the homepage')
-    console.log('post')
-    console.log(req.json)
-    //var jasonObject = JSON.parse(req.body)
-    res.json({msg: 'some message for post'})
+    var jasonObject = req.body
+    console.log(jasonObject)
+    var a = jasonObject['first-num']
+    var b = jasonObject['second-num']
+    var isnumber = true
+    var result = 0
+    if (isNaN(a) || isNaN(b)) {
+        isnumber = false
+        console.log('This is not number');
+    } else {
+        var c = a + b
+        // https://nodejs.org/api/crypto.html
+        const secret = 'abcdefg';
+        const hash = crypto.createHmac('sha256', secret)
+                   .update(c.toString())
+                   .digest('hex');
+        //console.log(hash);
+        result = hash
+
+    }
+    res.json({msg: result, isvalid:isnumber})
+
+    
 
 })
-  
+
 app.listen(port, () => {console.log(`Example app listening at http://localhost:${port}`)})
