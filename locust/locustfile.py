ebecb0253f8cbac16896bba536f287a5e1f179f1
sh3rLock3d <alipourghsaemi>
2020-11-02 22:52:45 +0330

    locust

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -1,17 +1,24 @@
 import time
+import random
 from locust import HttpUser, task, between
+import json
 
+myheaders = {'Content-Type': 'application/json'}
 class QuickstartUser(HttpUser):
     wait_time = between(1, 2)
 
-#    @task
-#    def index_page(self):
-#        self.client.get("go/write?line=5")
-#        self.client.get("/world")
-
-    @task(3)
-    def view_item(self):
+    @task
+    def view_(self):
         for item_id in range(10):
-            self.client.get("/go/write?line=5")
-            time.sleep(1)
-
+            if bool(random.getrandbits(1)):
+                a = "http://localhost:3000/nodejs/write?line=" + str(random.randint(2, 90))
+                self.client.get(a)
+                time.sleep(1)
+            else:
+                data = "{first-num:5,second-num:7}"
+                data = [{
+                    "first-num":random.randint(1,100),
+                    "second-num":random.randint(1,100)
+                }]
+                self.client.post("http://localhost:3000/nodejs/sha256", data=json.dumps(data), headers=myheaders)
+                time.sleep(1)
